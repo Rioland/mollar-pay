f5d562c5a5a49e01e12524fc3334ba0ee042b556
Adedamola Rioland <riolandadedamola@gmail.com>
2022-08-03 08:23:27 +0100

    v5

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <link rel="stylesheet" href="./css/footer.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
-<div class='container-fluid'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- <div class='container-fluid'>
+
         <div class="card">
             <div class="heading text-center">
                 <div class="head1">Just Scratching the Surface</div>
@@ -103,5 +105,71 @@
             </div>
             
         </div>
+    </div> -->
+
+
+<div class=" container myfotercontainer  mt-5">
+
+    <p class="h1 text-white text-center mt-5 pt-5">Download the app</p>
+    <p class="text-muted text-center mb-5 pb-5 w-50 ms-auto me-auto fw-3 fs-3">Join about 2 million people who buy sell, pay & get paid with Pocket app. No commission on any payments you receive.</p>
+    <div class="ms-auto me-auto mb-5 pb-5 mbv d-flex flex-row flex-md-column flex-sm-column flex-lg-row justify-content-center" style="width: fit-content;">
+        <button class="btn btn-light bbt me-4 mb-sm-3 mb-md-3"> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 28" style="margin-left:12px; margin-right:12px;height:1.6875em;margin-bottom:12px;">
+                <path fill="#212337" d="M19.995 23.908c-1.12 1.646-2.278 3.282-4.106 3.316-1.8.034-2.377-1.072-4.43-1.072-2.056 0-2.7 1.038-4.399 1.106-1.765.067-3.108-1.777-4.237-3.416-2.306-3.35-4.066-9.473-1.699-13.605 1.173-2.051 3.273-3.35 5.55-3.384 1.733-.032 3.371 1.175 4.43 1.175 1.06 0 3.049-1.45 5.138-1.237.875.037 3.33.355 4.907 2.674-.124.082-2.93 1.723-2.896 5.142.034 4.084 3.56 5.441 3.6 5.46-.031.096-.563 1.938-1.858 3.84ZM12.317 2.305C13.285 1.165 14.92.317 16.27.26c.173 1.584-.461 3.17-1.397 4.312-.938 1.14-2.474 2.03-3.98 1.91-.205-1.549.554-3.164 1.424-4.177Z"></path>
+            </svg> <span class="h2 ms-2">App Store</span> </button>
+
+
+
+        <button class="btn btn-light bbt me-4 mb-sm-3 mb-md-3">
+
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 30" style="margin-left:12px; margin-right:12px;height:1.6875em;margin-bottom:12px;">
+                <g fill="#212337" stroke="#E8E9EB" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.686">
+                    <path d="M1.42 1.85c-.315.314-.42.839-.42 1.469v23.202c0 .63.21 1.155.525 1.47l.104.105 13.009-13.018v-.21L1.42 1.849zm17.519 17.637-4.301-4.304v-.315l4.3-4.305.106.105 5.14 2.94c1.469.84 1.469 2.205 0 3.044l-5.245 2.835h0z"></path>
+                    <path d="m18.939 19.382-4.406-4.41L1.42 28.097c.524.525 1.258.525 2.203.105l15.316-8.819m0-8.819L3.623 1.85c-.945-.525-1.679-.42-2.203.105l13.113 13.019 4.406-4.41h0z"></path>
+                </g>
+            </svg>
+            <span class="h2 ms-2">play Store</span> </button>
+
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</div>
+
+
+<div class="container ms-auto me-auto mb-5 " style="width: fit-content;">
+    <button class="btn btn-light bbt me-4 mt-sm-3 t-md-3" style="background-color: whitesmoke;"><span class="h2 ms-2">FQA's</span> </button>
+    <button class="btn btn-light bbt me-4 mt-sm-3 mt-md-3" style="background-color: whitesmoke;"><span class="h2 ms-2">Privacy's</span> </button>
+    <button class="btn btn-light bbt me-4 mt-sm-3 mt-md-3" style="background-color: whitesmoke;"><span class="h2 ms-2">Team's</span> </button>
+    <button class="btn btn-light bbt me-4 mt-sm-3 mt-md-3" style="background-color: whitesmoke;"><span class="h2 ms-2">Contact Us</span> </button>
+
+</div>
+<hr class="text-muted">
+<div class=" container mt-5">
+    <h3 class="text-muted text-center">© 2022 moolapay Technologies.</h3>
+    <p class="ms-auto me-auto  text-muted text-center h5 w-50">Pocket App is a social commerce platform by <span class="h3">moolapay</span>. <br>
+        Banking services provided by <span class="h3">moolapay.</span></p>
+</div>
+<div class="container ms-auto me-auto mt-5 mb-5" style="width: fit-content;">
+    <div class="logo">
+        <a href="/">
+            <!-- <img class="img-logo1" src="images/logo_logo icon blue copy.png" width="80px" height="60px" alt=""> -->
+            <img class="img-logo2" src="images/logo_text blue only copy.png"  width="80px" height="60px"  alt="">
+        </a>
+    </div>
+</div>
+<div class="container ms-auto me-auto mt-5 mb-5" style="width: fit-content;">
+<svg xmlns="http://www.w3.org/2000/svg" width="28" height="27" fill="currentColor" class="bi bi-instagram me-3" viewBox="0 0 16 16">
+  <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z"/>
+</svg>
+
+<svg xmlns="http://www.w3.org/2000/svg" width="28" height="27"  fill="currentColor" class="bi bi-twitter me-3" viewBox="0 0 16 16">
+  <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z"/>
+</svg>
+
+<svg xmlns="http://www.w3.org/2000/svg" width="28" height="27" fill="currentColor" class="bi bi-facebook me-3" viewBox="0 0 16 16">
+  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
+</svg>
+
+<svg xmlns="http://www.w3.org/2000/svg" width="28" height="27" fill="currentColor" class="bi bi-whatsapp " viewBox="0 0 16 16">
+  <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+</svg>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
